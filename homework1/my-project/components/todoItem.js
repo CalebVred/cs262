990868b9d86db16ae47e346bd6e61bbe64f57efd
Caleb Vredevoogd <cvredevoogd99@gmail.com>
2020-09-19 10:06:08 -0400

    Adds homework 1

---
+++ homework1/my-project/components/todoItem.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function TodoItem({ pressHandler, item }) {
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+      <View style={styles.item}>
+        <MaterialIcons name='delete' size={18} color='#333'/>
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+};
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10
+  },
+
+  itemText: {
+    marginLeft: 10,
+  },
+})
