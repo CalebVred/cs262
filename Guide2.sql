2bef0f59b9e42b8be1b16723f20e561a2ad626b8
Caleb Vredevoogd <cvredevoogd99@gmail.com>
2020-10-28 19:22:18 -0400

    Update content

--- Guide2.sql
+++ Guide2.sql
@@ -12,7 +12,7 @@ GRANT SELECT ON Book to PUBLIC;
 INSERT INTO Book VALUES ('978-0995435001', 'The Ghost of Emily', 'James Fox Higgins', 29.99);
 INSERT INTO Book VALUES ('978-0316380522', 'The Art of Invisibility', 'Kevin Mitnick', 17.99);
 INSERT INTO Book VALUES ('978-0-912986-45-6', 'The Creature from Jekyll Island', 'G. Edward Griffin', 30.75);
-INSERT INTO Book VALUES ('9781612185439', 'Casino Royale', 'Ian Fleming', 14.95);
+INSERT INTO Book VALUES ('978-1612185439', 'Casino Royale', 'Ian Fleming', 14.95);
 INSERT INTO Book VALUES ('978-0913836613', 'The Holy Fire', 'Robert Payne', 26.00);
 INSERT INTO Book VALUES ('1587317753', 'The Slaughter of Cities', 'E. Michael Jones', 29.00);
 
